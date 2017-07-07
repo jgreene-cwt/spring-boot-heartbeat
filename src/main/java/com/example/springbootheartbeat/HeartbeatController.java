2617ff826ba0d5df362bb169b43944c550e8767b
Greene <UJXG9A5@carlsonwagonlit.com>
2017-07-07 09:35:10 -0500

    New commit for spring boot heartbeat application

---
+++ src/main/java/com/example/springbootheartbeat/HeartbeatController.java
@@ -0,0 +1,38 @@
+package com.example.springbootheartbeat;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HeartbeatController
+{
+
+    private static final String template = "This Spring Boot sample application is up and running, %s!";
+
+    @RequestMapping("/")
+    public String index()
+    {
+        return "This Spring Boot sample application is up and running!";
+    }
+    
+    @RequestMapping(value = "/greeting/{name}", method = RequestMethod.GET)
+    public String heartbeat(@PathVariable String name)
+    {
+        return String.format(template, name);
+        //return this.getCurrentDateTimeStamp() + ": " + String.format(template, name);        
+    }
+    
+    private String getCurrentDateTimeStamp()
+    {
+        long currentMilliseconds = System.currentTimeMillis();
+        SimpleDateFormat sdf = new SimpleDateFormat("MMM dd,yyyy HH:mm");    
+        Date resultdate = new Date(currentMilliseconds);
+        
+        return sdf.format(resultdate);
+    }
+}
---
+++ src/main/java/com/example/springbootheartbeat/SpringBootHeartbeatApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootheartbeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHeartbeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHeartbeatApplication.class, args);
+	}
+}
