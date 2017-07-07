e627161ef54c24532cff8afcc7a6a9105b8e94a1
Greene <UJXG9A5@carlsonwagonlit.com>
2017-07-07 10:38:37 -0500

    modified pom

--- src/main/java/com/example/springbootheartbeat/SpringBootHeartbeatApplication.java
+++ src/main/java/com/example/springbootheartbeat/SpringBootHeartbeatApplication.java
@@ -2,11 +2,20 @@ package com.example.springbootheartbeat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootHeartbeatApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootHeartbeatApplication.class, args);
-	}
+public class SpringBootHeartbeatApplication extends SpringBootServletInitializer
+{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(SpringBootHeartbeatApplication.class);
+    }
+    
+    public static void main(String[] args) throws Exception
+    {
+        SpringApplication.run(SpringBootHeartbeatApplication.class, args);
+    }
 }
